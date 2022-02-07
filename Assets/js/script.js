18d188ab558be9c094237ab1bf8cde14c7b74a09
David Warner <david.hans.warner@gmail.com>
2022-02-07 07:52:38 -0700

    more changes to js, trying to save searches to local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,5 @@
 // define variables
-var city = "";
+var cityList =$("#city-list");
 var searchedCities = [];
 var date = moment().format('dddd, MMMM Do YYYY');
 var dateTime = moment().format('YYYY-MM-DD HH:MM:SS')
@@ -13,109 +13,44 @@ var APIKey = "b78502564bdb2b68696e0c9f19ef03ea";
 
 // functions 
 
-// clears out local storage 
-// function deleteStorage () {
-//     localStorage.clear();
-//   };
 
-// save previous searches to local storage 
+// save previous searches to local storage and
 
-$('.search-button').on("click", function (event) {
+init();
+
+function init (){
+
+    var savedSearches = JSON.parse(localStorage.getItem("searchedCities"));
+
+    if (searchedCities === "") {
+        searchedCities = savedSearches;
+    }
+
+    displaySearches();
+
+}
+
+$("#city-button").on("click", function(event) {
     event.preventDefault();
-    city = city.parent(".searchBtn").sibilings(".textVal").val().trim();
+
+    var city = $("#search-input").val().trim();
+
     if (city === "") {
-        return; 
-    };
-    cities.push(city);
-
-    localStorage.setItem("city", JSON.stringify(searchedCities));
-    fiveDayEl.empty();
-
-    
-    // save search to the side bar 
-    getPreviousSearches();
-    // displays selected cities weather to the main card as well as five day forecast 
-    getCurrentWeather();
-});
-
-
-var searchedCities = $(".searchedCities");
-
-function getSearchedCities() {
-    searchedCities.empty();
-
-    for (let i = 0; i < searchedCities.length; i++) {
-
-        var rowEl = $("<row>");
-        var btnEl = $("<button>").text(`${searchedCities[i]}`)
-
-        rowEl.addClass("row searchedBtnRow");
-        btnEl.addClass("btn searchedBtn");
-
-        searchedCities.prepend(rowEl);
-        rowEl.append(btnEl);
-    } if (!city) {
         return;
     }
 
-    $('.searchedBtn').on("click", function (event) {
-		event.preventDefault();
-		city = $(this).text();
-		fiveForecastEl.empty();
-		getWeatherToday();
-	});
+    searchedCities.push(city);
+
+    // saveSearches();
+    // displaySearches();
+
+});
+
+function displaySearches() {
+
+}
+
+function saveSearches() {
+    localStorage.setItem("searchedCities", JSON.stringify(searchedCities));
+    console.log(localStorage);
 };
-
-// Displays weather data to the main card 
-
-function getCurrentWeather() {
-    var openweatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&apikey=${APIKey}";
-
-    $(cardTodayBody).empty();
-
-    fetch({
-
-    }).then(function (response) {
-		$('.cardTodayCityName').text(response.name);
-		$('.cardTodayDate').text(date);
-		//Icons
-		$('.icons').attr('src', `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`);
-		// Temperature
-		var pEl = $('<p>').text(`Temperature: ${response.main.temp} °F`);
-		cardTodayBody.append(pEl);
-		//Feels Like
-		var pElTemp = $('<p>').text(`Feels Like: ${response.main.feels_like} °F`);
-		cardTodayBody.append(pElTemp);
-		//Humidity
-		var pElHumid = $('<p>').text(`Humidity: ${response.main.humidity} %`);
-		cardTodayBody.append(pElHumid);
-		//Wind Speed
-		var pElWind = $('<p>').text(`Wind Speed: ${response.wind.speed} MPH`);
-		cardTodayBody.append(pElWind);
-		//Set the lat and long from the searched city
-		var cityLon = response.coord.lon;
-		// console.log(cityLon);
-		var cityLat = response.coord.lat;
-		// console.log(cityLat);
-
-		var getUrlUvi = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&exclude=hourly,daily,minutely&appid=${key}`;
-
-		fetch({
-			url: getUrlUvi,
-			method: 'GET',
-		}).then(function (response) {
-			var pElUvi = $('<p>').text(`UV Index: `);
-			var uviSpan = $('<span>').text(response.current.uvi);
-			var uvi = response.current.uvi;
-			pElUvi.append(uviSpan);
-			cardTodayBody.append(pElUvi);
-        });
-        getFiveDayForecast();
-    });
-};
-
-
-
-
-
-// save buttons for previous searches
