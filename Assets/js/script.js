b51ec74883d70ec213d39aa9ca60e000e59cffae
David Warner <david.hans.warner@gmail.com>
2022-02-08 07:54:38 -0700

    added weather API and I'm able to pull info to the DOM. Still a lot of work that needs to be done

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,9 +1,39 @@
 // // define variables
 
 var search = document.querySelector('#search');
-var city = document.querySelector('#city');
+var inputValue = document.querySelector('.inputValue');
 var cities = document.querySelector('#cities');
+var button = document.querySelector('.button');
+var nameInput = document.querySelector('.name');
+var temp = document.querySelector('.temp');
+var wind = document.querySelector('.wind');
+var humidity = document.querySelector('.humidity');
+var uvIndex = document.querySelector('.uvIndex'); 
 
+var key = 'b78502564bdb2b68696e0c9f19ef03ea'
+
+button.addEventListener('click', function(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value.trim()+'&appid=b78502564bdb2b68696e0c9f19ef03ea')
+    .then(response => response.json())
+    .then(data => {
+        var nameValue = data['nameInput'];
+        var tempValue = data['main']['temp'];
+        var windValue = data['wind'] 
+        var humidityValue = data['main']['humidity'];
+        // var uvIndexValue = data ['main']['']
+
+        nameInput.innerHTML =nameValue;
+        temp.innerHTML =tempValue;
+        wind.innerHTML =windValue;
+    
+    })
+
+.catch(err => alert('Wrong city name!'))
+})
+
+// current date / time  
+var date = moment().format('dddd, MMMM Do YYYY');
+var dateTime = moment().format('YYYY-MM-DD HH:MM:SS')
 
 // save search items to local storage 
 
@@ -13,14 +43,16 @@ search.addEventListener('submit', function (event) {
 	event.preventDefault();
 
 	// Ignore it if the wishlist item is empty
-	if (city.value.length < 1) return;
+	if (inputValue.value.length < 1) return;
 
-    cities.innerHTML += '<ul>' + city.value + '<ul>';
+    cities.innerHTML += '<ul>' + inputValue.value + '<ul>';
 
 	// Clear input
-	city.value = '';
+	inputValue.value = '';
 
 	// Save the list to localStorage
 	localStorage.setItem('city', cities.innerHTML);
 
+
 }, false);
+
